bcca65ccb211237f55e55d6ea7239926d6675f21
Glenn Slear <glennstreetman@gmail.com>
2022-08-26 10:25:48 -0700

    add new test

--- runTests.js
+++ runTests.js
@@ -1,8 +1,9 @@
-import {copyFromOneToManySheets} from './tests/copyFromOneToManySheets/test.js'
-import {copyManyToOne} from './tests/copyManyToOne/test.js'
-import {copyNoOverrides} from './tests/noOverridesOneToOne/test.js'
+import { copyFromOneToManySheets } from "./tests/copyFromOneToManySheets/test.js";
+import { copyManyToOne } from "./tests/copyManyToOne/test.js";
+import { copyNoOverrides } from "./tests/noOverridesOneToOne/test.js";
+import { overrides } from "./tests/overrides/test.js";
 
-
-copyFromOneToManySheets()
-copyManyToOne()
-copyNoOverrides()
+copyFromOneToManySheets();
+copyManyToOne();
+copyNoOverrides();
+overrides();
